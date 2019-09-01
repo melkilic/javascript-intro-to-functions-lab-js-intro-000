89545c534658d9b0230ccedac2d581580cc61e61
Melike KILIC <melike180400@gmail.com>
2019-09-01 20:19:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function whisper(string){
   if(string===string.toLowerCase);
   return "I can\'t hear you!" ;
 }
-function sayHiToGrandma(string){
-if(string===string.toUpperCase)
+{if(string===string.toUpperCase)
  return "YES INDEED! " ;
  }
